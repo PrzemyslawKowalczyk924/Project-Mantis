fb1850adaf13e756dcd7c025497f5b1d66465429
Komputer <kowalczyk.przemek@wp.pl>
2024-03-21 04:06:38 +0100

    Add nav & footer plus remove vite demo

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  resolve: {
+    /*something*/
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
