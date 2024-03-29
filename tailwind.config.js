441ef5205963f112ff40c7b37a2919b60442891b
Komputer <kowalczyk.przemek@wp.pl>
2024-03-29 15:47:23 +0100

    Beging to work with main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy-blue': '#C5E7F1',
+      },
+      borderRadius: {
+        'borderRadiusForShip': '41% 32% 25% 36% / 55% 0% 0% 32%',
+      },
+      backgroundSize: {
+        '50%': '50%',
+      }
+    },
   },
   plugins: [],
 }
